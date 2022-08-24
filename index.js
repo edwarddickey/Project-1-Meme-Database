dee1889ac3337fdf8cba210414171643c4f1589b
Edward Dickey <edward.c.dickey@gmail.com>
2022-08-24 13:03:40 -0600

    adding CSS

--- index.js
+++ index.js
@@ -75,3 +75,13 @@ function allBtnAct(){
 allBtn.addEventListener('click',(e)=>{
     allBtnAct();
 })
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
